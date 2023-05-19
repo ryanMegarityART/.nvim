fc41fa76fff3c0c3cbf1ce92e4a19068846c3652
ryan <ryanmegarity@gmail.com>
2023-05-19 12:26:29 +0100

    added mappings

--- lua/megar/remap.lua
+++ lua/megar/remap.lua
@@ -1,6 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
 -- move lines up and down when highlighted
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
